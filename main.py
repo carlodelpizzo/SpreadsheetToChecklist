2e471b5587c1f83940f0a7337533283cd31ac7f0
carlodelpizzo <carlodelpizzo@gmail.com>
2022-09-19 22:38:13 -0400

    blah blah

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from openpyxl.drawing.image import Image
 from openpyxl.drawing.spreadsheet_drawing import OneCellAnchor, AnchorMarker
 from openpyxl.utils.units import pixels_to_EMU, cm_to_EMU
 from openpyxl.drawing.xdr import XDRPositiveSize2D
-from openpyxl.styles import Font
-from openpyxl.styles.borders import Border, Side
+from openpyxl.styles import Font, PatternFill
+# from openpyxl.styles.borders import Border, Side
 from barcode import Code128  # Package called python-barcode
 from barcode.writer import ImageWriter  # Requires Pillow
 from tkinter import *
@@ -41,9 +41,6 @@ class ChecklistProgram:
         self.open_file_button = tk.Button(self.root, text='Browse...', width=10, command=self.open_file, takefocus=0)
         self.open_file_button.pack()
 
-        self.test_button = tk.Button(self.root, text='Test', width=10, command=self.test_barcode, takefocus=0)
-        self.test_button.pack()
-
         # Version Label
         self.version_label = Label(self.root, text='ver ' + version)
         self.version_label.place(relx=1, rely=1.01, anchor='se')
@@ -71,78 +68,103 @@ class ChecklistProgram:
     def process_file(self, file_path: str):
         workbook = openpyxl.load_workbook(file_path)
         sheet = workbook.active
-        part_col = None
-        type_col = None
-        material_col = None
-        break_col = None
-        weld_col = None
-        powder_col = None
-        quantity_col = None
-        batch_col = None
+        part_col, type_col, material_col, break_col, weld_col, powder_col, quantity_col, batch_col, setup_col = \
+            None, None, None, None, None, None, None, None, None
         read_col_labels = False
-        all_col_read = False
+        labels_row = None
         for r in range(1, sheet.max_row + 1):
             current_part = None
             for c in range(1, sheet.max_column + 1):
                 cell_obj = sheet.cell(row=r, column=c)
                 if read_col_labels:
+                    if r != labels_row:
+                        read_col_labels = False
+                    elif (cell_val := cell_obj.value) == 'Part':
+                        part_col = c
+                    elif cell_val == 'Type':
+                        type_col = c
+                    elif cell_val == 'Material':
+                        material_col = c
+                    elif cell_val == 'Break':
+                        break_col = c
+                    elif cell_val == 'Weld':
+                        weld_col = c
+                    elif cell_val == 'PowdCoat Y/N':
+                        powder_col = c
+                    elif cell_val == 'Quantity':
+                        quantity_col = c
+                    elif cell_val == 'Batch':
+                        batch_col = c
+                    elif cell_val == 'Setup':
+                        setup_col = c
+                    if read_col_labels:
+                        continue
+                if part_col and c == part_col and cell_obj.value:
+                    current_part = cell_obj.value
+                    self.parts_list.append(current_part)
+                    self.parts[current_part] = {}
+                elif current_part:
+                    if type_col and c == type_col:
+                        self.parts[current_part]['type'] = cell_obj.value
+                    elif material_col and c == material_col:
+                        self.parts[current_part]['material'] = cell_obj.value
+                    elif break_col and c == break_col:
+                        if cell_obj.value == 'N':
+                            self.parts[current_part]['break'] = False
+                        else:
+                            self.parts[current_part]['break'] = True
+                    elif weld_col and c == weld_col:
+                        if cell_obj.value == 'N':
+                            self.parts[current_part]['weld'] = False
+                        else:
+                            self.parts[current_part]['weld'] = True
+                    elif powder_col and c == powder_col:
+                        if cell_obj.value == 'N':
+                            self.parts[current_part]['powder'] = False
+                        else:
+                            self.parts[current_part]['powder'] = True
+                    elif quantity_col and c == quantity_col:
+                        self.parts[current_part]['quantity'] = cell_obj.value
+                    elif batch_col and c == batch_col and self.batch is None:
+                        self.batch = cell_obj.value
+                    elif setup_col and c == setup_col:
+                        self.parts[current_part]['setup'] = cell_obj.value
+                if cell_obj.value == '#' or cell_obj.value == 'Part':
                     if cell_obj.value == 'Part':
                         part_col = c
-                    elif cell_obj.value == 'Type':
-                        type_col = c
-                    elif cell_obj.value == 'Material':
-                        material_col = c
-                    elif cell_obj.value == 'Break':
-                        break_col = c
-                    elif cell_obj.value == 'Weld':
-                        weld_col = c
-                    elif cell_obj.value == 'PowdCoat Y/N':
-                        powder_col = c
-                    elif cell_obj.value == 'Quantity':
-                        quantity_col = c
-                    elif cell_obj.value == 'Batch':
-                        batch_col = c
-                    else:
-                        all_col_read = True
-                        column_vars = [part_col, type_col, material_col, break_col, weld_col, powder_col, quantity_col,
-                                       batch_col]
-                        for column_var in column_vars:
-                            if column_var is None:
-                                all_col_read = False
-                                break
-                        if all_col_read:
-                            read_col_labels = False
-                elif all_col_read:
-                    if c == part_col and cell_obj.value:
-                        current_part = cell_obj.value
-                        self.parts_list.append(current_part)
-                        self.parts[current_part] = {}
-                    elif current_part:
-                        if c == type_col:
-                            self.parts[current_part]['type'] = cell_obj.value
-                        elif c == material_col:
-                            self.parts[current_part]['material'] = cell_obj.value
-                        elif c == break_col:
-                            if cell_obj.value == 'N':
-                                self.parts[current_part]['break'] = False
-                            else:
-                                self.parts[current_part]['break'] = True
-                        elif c == weld_col:
-                            if cell_obj.value == 'N':
-                                self.parts[current_part]['weld'] = False
-                            else:
-                                self.parts[current_part]['weld'] = True
-                        elif c == powder_col:
-                            if cell_obj.value == 'N':
-                                self.parts[current_part]['powder'] = False
-                            else:
-                                self.parts[current_part]['powder'] = True
-                        elif c == quantity_col:
-                            self.parts[current_part]['quantity'] = cell_obj.value
-                        elif c == batch_col and self.batch is None:
-                            self.batch = cell_obj.value
-                if cell_obj.value == '#':
                     read_col_labels = True
+                    labels_row = r
+        if setup_col:
+            part_types, temp_dict, check_list, setup_list, sorted_parts, no_setup_parts = [], {}, [], [], [], []
+            for part in self.parts_list:
+                if self.parts[part]['type'] and (part_type := self.parts[part]['type']) not in part_types:
+                    part_types.append(part_type)
+            part_types.sort()  # Sort by type
+            for part_type in part_types:
+                temp_dict[part_type] = []
+                for part in self.parts_list:
+                    if self.parts[part]['type'] and self.parts[part]['type'] == part_type:
+                        temp_dict[part_type].append(part)
+                        check_list.append(part)
+            # Check for missed parts
+            for part in self.parts_list:
+                if part not in check_list:
+                    no_setup_parts.append(part)
+            parts_list_bkp = [x for x in self.parts_list]
+            self.parts_list = []
+            for part_type in part_types:
+                for part in temp_dict[part_type]:
+                    if self.parts[part]['setup'] and (part_setup := self.parts[part]['setup']) not in setup_list:
+                        setup_list.append(part_setup)
+                setup_list.sort()  # Sort by setup
+                for setup in setup_list:
+                    for part in temp_dict[part_type]:
+                        if self.parts[part]['setup'] and self.parts[part]['setup'] == setup:
+                            self.parts_list.append(part)
+            self.parts_list.extend(no_setup_parts)
+            for part in parts_list_bkp:
+                if part not in self.parts_list:
+                    self.parts_list.append(part)
 
         self.make_break_list()
 
@@ -154,15 +176,15 @@ class ChecklistProgram:
         sheet['A1'].value = 'Batch: ' + str(self.batch)
         sheet['A1'].font = Font(bold=True)
         sheet.cell(row=1, column=1).alignment = openpyxl.styles.Alignment(horizontal='center')
-        sheet['D1'].value = 'Notes'
-        sheet['D1'].font = Font(bold=True)
-        sheet.cell(row=1, column=4).alignment = openpyxl.styles.Alignment(horizontal='center')
-        i = 2
+        # sheet['D1'].value = 'Notes'
+        # sheet['D1'].font = Font(bold=True)
+        # sheet.cell(row=1, column=4).alignment = openpyxl.styles.Alignment(horizontal='center')
         temp_dir = self.cur_dir + 'barcode_temp/'
         if os.path.isdir(temp_dir):
             shutil.rmtree(temp_dir)
         os.mkdir(temp_dir)
-        for part in self.parts_list:
+        alt_bg_color = 'EEEEEE'
+        for i, part in enumerate(self.parts_list, start=2):
             part_name = part + ' ['
             if self.parts[part]['break']:
                 part_name += 'B'
@@ -174,32 +196,38 @@ class ChecklistProgram:
                 if part_name[-1] != '[':
                     part_name += '-'
                 part_name += 'P'
-            if part_name[-1] == '[':
+            if part_name.endswith('['):
                 part_name = part_name[0:-2]
             else:
                 part_name += ']'
+            if self.parts[part]['setup']:
+                part_name = ''.join([part_name, ' {', self.parts[part]['setup'], '}'])
             sheet.cell(row=i, column=3).alignment = openpyxl.styles.Alignment(vertical='top')
             sheet.cell(row=i, column=3).value = part_name
-            sheet.cell(row=i, column=1).alignment = openpyxl.styles.Alignment(horizontal='right', vertical='center')
-            sheet.cell(row=i, column=1).value = '☐'
+            sheet.cell(row=i, column=2).alignment = openpyxl.styles.Alignment(horizontal='right', vertical='center')
+            sheet.cell(row=i, column=2).value = '☐'
             with open(temp_dir + part + '.png', "wb") as file:
-                Code128(part, writer=ImageWriter()).write(file, options={'write_text': False})
+                if i % 2 != 0:
+                    Code128(part, writer=ImageWriter()).write(file, options={'write_text': False})
+                else:
+                    Code128(part, writer=ImageWriter()).write(file, options={'write_text': False,
+                                                                             'background': ''.join([
+                                                                                 '#', alt_bg_color])})
             img = openpyxl.drawing.image.Image(temp_dir + part + '.png')
             height = 18
-            width = 210
+            width = 280
             img.height = height
             img.width = width
             # Silly nonsense below
             row_offset = 0.25
-            marker = AnchorMarker(col=1, row=i-1, rowOff=cm_to_EMU((row_offset * 49.77) / 99),
+            marker = AnchorMarker(col=0, row=i-1, rowOff=cm_to_EMU((row_offset * 49.77) / 99),
                                   colOff=cm_to_EMU((0.1 * 49.77) / 99))
             size = XDRPositiveSize2D(pixels_to_EMU(width), pixels_to_EMU(height))
             img.anchor = OneCellAnchor(_from=marker, ext=size)
             sheet.add_image(img)
-            i += 1
         dims = {}
-        border = Border(left=Side(style='thin'), right=Side(style='thin'),
-                        top=Side(style='thin'), bottom=Side(style='thin'))
+        # border = Border(left=Side(style='thin'), right=Side(style='thin'),
+        #                 top=Side(style='thin'), bottom=Side(style='thin'))
         for row in sheet.rows:
             for cell in row:
                 if cell.value:
@@ -208,20 +236,18 @@ class ChecklistProgram:
             sheet.column_dimensions[col].width = value + 2
         for row in range(2, sheet.max_row + 1):
             sheet.row_dimensions[row].height = 20
-            sheet.cell(row=row, column=4).border = border
-        sheet.column_dimensions['A'].width = 5
-        sheet.column_dimensions['B'].width = 30
-        sheet.column_dimensions['D'].width = 20
+            if row % 2 == 0:
+                for col in ['A', 'B', 'C']:
+                    sheet[''.join([col, str(row)])].fill = PatternFill(start_color=alt_bg_color, end_color=alt_bg_color,
+                                                                       fill_type='solid')
+        #     sheet.cell(row=row, column=4).border = border
+        sheet.column_dimensions['A'].width = 42
+        sheet.column_dimensions['B'].width = 2
+        # sheet.column_dimensions['D'].width = 20
         workbook.save('test.xlsx')
         if os.path.isdir(temp_dir):
             shutil.rmtree(temp_dir)
         print('DONE')
-
-    # noinspection PyMethodMayBeStatic
-    def test_barcode(self):
-        with open('test.png', "wb") as file:
-            Code128('This is a test', writer=ImageWriter()).write(file, options={'write_text': False})
-        self.make_break_list()
 
 
 ChecklistProgram()
